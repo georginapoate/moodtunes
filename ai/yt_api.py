a056344142fe94666fcb23997b71a03c7c1552cb
georginapoate <georgina.poting@gmail.com>
2025-05-26 18:26:03 +0300

    updated gitignore (again) & deleted data files from repo

--- ai/yt_api.py
+++
@@ -1,8 +0,0 @@
-from ytmusicapi import YTMusic
-
-ytmusic = YTMusic()  # You can use anonymous requests, or auth if desired
-
-search_results = ytmusic.search("faded", filter="songs")
-
-for song in search_results[:10]:  # get top 10 results
-    print(f"{song['title']} by {song['artists'][0]['name']}")
